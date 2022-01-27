fe5f63f8f69d2c18bb57fa764c38baa5f7646e97
Julian Prester <julian@julianprester.com>
2022-01-27 12:15:41 +1100

    Fix browser sync settings

--- .eleventy.js
+++ .eleventy.js
@@ -3,25 +3,25 @@ const eleventyNavigationPlugin = require("@11ty/eleventy-navigation");
 const eleventyImagePlugin = require("@11ty/eleventy-img");
 
 async function imageShortcode(src, alt, sizes = "100vw") {
-    if(alt === undefined) {
-      // You bet we throw an error on missing alt (alt="" works okay)
-      throw new Error(`Missing \`alt\` on responsiveimage from: ${src}`);
-    }
-  
-    let metadata = await eleventyImagePlugin(src, {
-      widths: [300, 400, 500],
-      formats: ['webp', 'jpeg'],
-      urlPath: "/assets/cover/",
-      outputDir: "./public/assets/cover/"
-    });
-  
-    let lowsrc = metadata.jpeg[0];
-    let highsrc = metadata.jpeg[metadata.jpeg.length - 1];
-  
-    return `<picture>
+  if (alt === undefined) {
+    // You bet we throw an error on missing alt (alt="" works okay)
+    throw new Error(`Missing \`alt\` on responsiveimage from: ${src}`);
+  }
+
+  let metadata = await eleventyImagePlugin(src, {
+    widths: [300, 400, 500],
+    formats: ['webp', 'jpeg'],
+    urlPath: "/assets/cover/",
+    outputDir: "./public/assets/cover/"
+  });
+
+  let lowsrc = metadata.jpeg[0];
+  let highsrc = metadata.jpeg[metadata.jpeg.length - 1];
+
+  return `<picture>
       ${Object.values(metadata).map(imageFormat => {
-        return `  <source type="${imageFormat[0].sourceType}" srcset="${imageFormat.map(entry => entry.srcset).join(", ")}" sizes="${sizes}">`;
-      }).join("\n")}
+    return `  <source type="${imageFormat[0].sourceType}" srcset="${imageFormat.map(entry => entry.srcset).join(", ")}" sizes="${sizes}">`;
+  }).join("\n")}
         <img
           src="${lowsrc.url}"
           width="${highsrc.width}"
@@ -31,31 +31,38 @@ async function imageShortcode(src, alt, sizes = "100vw") {
           loading="lazy"
           decoding="async">
       </picture>`;
-  }
+}
 
 module.exports = function (eleventyConfig) {
-    eleventyConfig.addFilter("formatDate", (dateObj) => {
-        return DateTime.fromJSDate(dateObj).toFormat('LLLL yyyy');
-    });
+  eleventyConfig.addFilter("formatDate", (dateObj) => {
+    return DateTime.fromJSDate(dateObj).toFormat('LLLL yyyy');
+  });
 
-    eleventyConfig.addPlugin(eleventyNavigationPlugin);
-    eleventyConfig.addNunjucksAsyncShortcode("image", imageShortcode);
+  eleventyConfig.addPlugin(eleventyNavigationPlugin);
+  eleventyConfig.addNunjucksAsyncShortcode("image", imageShortcode);
 
-    eleventyConfig.setBrowserSyncConfig({
-        snippet: false,
-    });
-    eleventyConfig.addPassthroughCopy('src/assets/favicon');
-    eleventyConfig.addPassthroughCopy('src/assets/cv');
-    eleventyConfig.addPassthroughCopy('src/robots.txt');
-
-    return {
-        htmlTemplateEngine: "njk",
-        dir: {
-            input: 'src',
-            output: 'public',
-            layouts: 'layouts',
-            includes: 'includes',
-            data: 'data',
+  eleventyConfig.setBrowserSyncConfig({
+    snippetOptions: {
+      rule: {
+        match: /<\/body>/i,
+        fn: function (snippet, match) {
+          return snippet + match;
         }
+      }
     }
+  });
+  eleventyConfig.addPassthroughCopy('src/assets/favicon');
+  eleventyConfig.addPassthroughCopy('src/assets/cv');
+  eleventyConfig.addPassthroughCopy('src/robots.txt');
+
+  return {
+    htmlTemplateEngine: "njk",
+    dir: {
+      input: 'src',
+      output: 'public',
+      layouts: 'layouts',
+      includes: 'includes',
+      data: 'data',
+    }
+  }
 }
